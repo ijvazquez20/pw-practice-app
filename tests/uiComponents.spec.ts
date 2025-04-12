48b29e06d373e3fbffe9840469a07840138b00b6
ijvazquez20 <ij.vazquez20@gmail.com>
2025-04-12 11:34:42 -0300

    Page Objects

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -1,8 +1,8 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from "@playwright/test"
 
 
 test.beforeEach(async ({ page }) => {
-    await page.goto("http://localhost:4200/");
+    await page.goto("http://localhost:4200/")
 })
 
 test.describe("Form Layouts Page", () => {
