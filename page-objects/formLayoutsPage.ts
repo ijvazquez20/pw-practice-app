3eac40632f96fa9d16384c0ad9621916082ad284
ijvazquez20 <104858329+ijvazquez20@users.noreply.github.com>
2025-04-16 15:57:23 -0300

    Parameterized Methods

---
+++ page-objects/formLayoutsPage.ts
@@ -0,0 +1,39 @@
+import { Page } from "@playwright/test"
+
+export class formLayoutsPage {
+
+    private readonly page: Page
+
+    constructor(page: Page) {
+        this.page = page
+    }
+
+    async submitUsingTheGridFormWithCredentialsAndSelectOption(email: string, password: string, optionText: string) {
+        const usingTheGridForm = this.page.locator('nb-card', { hasText: "Using the Grid" })
+
+        await usingTheGridForm.getByRole('textbox', { name: "Email" }).fill(email)
+        await usingTheGridForm.getByRole('textbox', { name: "Password" }).fill(password)
+        await usingTheGridForm.getByRole('radio', { name: optionText }).check({ force: true })
+        await usingTheGridForm.getByRole('button').click()
+    }
+
+    /**
+     * This method will fill out the Inline Form with the name, email and checkbox.
+     * @param name - Name of the user. First and Last Name.
+     * @param email - Email of the user. Must be a valid email address.
+     * @param checkbox - True or false.
+     */
+    async submitInlineFormWithNameEmailAndCheckbox(name: string, email: string, checkbox: boolean){
+        const usingInlineForm = this.page.locator('nb-card', { hasText: "Using the Grid" })
+
+        await usingInlineForm.getByRole('textbox', { name: "Jane Doe" }).fill(name)
+        await usingInlineForm.getByRole('textbox', { name: "Email" }).fill(email)
+
+        if (checkbox){
+            await usingInlineForm.getByRole('checkbox').check({ force: true })
+        }
+
+        await usingInlineForm.getByRole('button').click()
+
+    }
+}
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test"
 import { NavigationPage } from "../page-objects/navigationPage"
+import { formLayoutsPage } from "../page-objects/formLayoutsPage"
 
 test.beforeEach(async ({ page }) => {
     await page.goto("http://localhost:4200/")
@@ -9,9 +10,19 @@ test.beforeEach(async ({ page }) => {
 
 test("Navigate to Form Page", async ({ page }) => {
     const navigateTo = new NavigationPage(page)
+
     await navigateTo.formLayoutsPage()
     await navigateTo.datepickerPage()
     await navigateTo.smartTablePage()
     await navigateTo.toastrPage()
     await navigateTo.tooltipPage()
+})
+
+test("Parametrized methods", async ({ page }) => {
+    const navigateTo = new NavigationPage(page)
+    const onFormLayoutsPage = new formLayoutsPage(page)
+
+    await navigateTo.formLayoutsPage()
+    await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption("test@mail.com", "Test123!", "Option 1")
+    await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox("John Test", "mail@test.com", true)
 })
